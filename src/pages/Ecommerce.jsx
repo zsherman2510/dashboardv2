9bf1383e0ea0d25ff3a317c70fa69f40c5717293
Zavion Sherman <zsherman2510@hotmail.com>
2022-07-27 13:12:21 -0700

    finished recent transactions

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -9,6 +9,12 @@ import { earningData, medicalproBranding, recentTransactions, weeklyStats, dropd
 import { useStateContext } from '../contexts/ContextProvider';
 import product9 from '../data/product9.jpg';
 
+const DropDown = ({ currentMode }) => (
+  <div className="w-28 border-1 border-color px-2 py-1 rounded-md">
+    <DropDownListComponent id="time" fields={{ text: 'Time', value: 'Id' }} style={{ border: 'none', color: (currentMode === 'Dark') && 'white' }} value="1" dataSource={dropdownData} popupHeight="220px" popupWidth="120px" />
+  </div>
+);
+
 const Ecommerce = () => {
   const { currentColor, currentMode } = useStateContext();
   return (
@@ -112,7 +118,60 @@ const Ecommerce = () => {
               <Stacked currentMode={currentMode} width="320px" height="360px"/>
             </div>
           </div>
-          
+        </div>
+        <div>
+          <div className='rounded-2xl md:w-400 p-4 m-3' style={{ backgroundColor: currentColor }}>
+            <div className="flex justify-between items-center">
+              <p className="font-semibold text-white text-2xl">Earnings</p>
+              <div>
+                <p className="text-2xl text-white font-semibold mt-8">$63,448.68</p>
+                <p className='text-gray-200'>Monthly Revenue</p>
+              </div>
+            </div>
+            <div className="mt-4">
+              <SparkLine currentColor={currentColor} id="column-sparkLink" height="100px" type="Column" data={SparklineAreaData} width="320" color="rgb(242, 252, 253)"/>
+            </div>
+          </div>
+          <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl md:w-400 p-8 m-3 flex justify-center items-center gap-10">
+            <div>
+              <p className="text-2xl font-semibold">$43,232</p>
+              <p className="text-gray-400">Yearly sales</p>
+            </div>
+            <div className="w-40">
+              <Pie id="pie-chart" data={ecomPieChartData} legendVisibility={false} height="160px" />
+            </div>
+          </div>
+        </div>
+        <div className="flex gap-10 m-4 flex-wrap justify-center">
+          <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg p-6 rounded-2xl">
+            <div className="flex justify-between items-center gap-5">
+              <p className="text-xl font-semibold">Recent Transactions</p>
+              <DropDown currentMode={currentMode}/>
+            </div>
+            <div className="mt-10 w-72 md:w-400">
+            {recentTransactions.map((item) => (
+              <div key={item.title} className="flex justify-between mt-4">
+                <div className="flex gap-4">
+                  <button
+                    type="button"
+                    style={{
+                      color: item.iconColor,
+                      backgroundColor: item.iconBg,
+                    }}
+                    className="text-2xl rounded-lg p-4 hover:drop-shadow-xl"
+                  >
+                    {item.icon}
+                  </button>
+                  <div>
+                    <p className="text-md font-semibold">{item.title}</p>
+                    <p className="text-sm text-gray-400">{item.desc}</p>
+                  </div>
+                </div>
+                <p className={`text-${item.pcColor}`}>{item.amount}</p>
+              </div>
+            ))}
+          </div>
+          </div>
         </div>
       </div>
     </div>
